cd0fa2386b12602afbb98f60a2701f0b488a9856
Siripoom <artorsiriratpoom@gmail.com>
2024-03-23 15:14:40 +0700

    created Footer commponent

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ import Nav from  "./components/Nav"
 import Home from "./pages/Home"
 import Translate from './pages/Translate'
 import Grammar from './pages/Grammar'
+import Footer from './components/Footer'
 function App() {
   return (
     <>
@@ -16,6 +17,7 @@ function App() {
           <Route path="grammar" element={<Grammar/>} />
           <Route path="*" element={<Page404/>} />
         </Routes>
+        
       </BrowserRouter>
     </>
   )
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,10 +1,12 @@
 import SearchBar from "../components/SearchBar";
 import Hero from "../components/Hero"
+import Footer from "../components/Footer"
 export default function Home(){
     return(
         <div className="container">
             <Hero/>
             <SearchBar/>
+            <Footer/>
         </div>
     )
 }
