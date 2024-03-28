892466e419898017cfa7be8585cd0c080fe925de
Siripoom <artorsiriratpoom@gmail.com>
2024-03-28 23:46:30 +0700

    new version

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,18 @@
-import './App.css'
-import Register from './pages/Register'
-import About from './pages/About'
-import { BrowserRouter,Routes,Route } from 'react-router-dom'
+import "./App.css";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./pages/home";
+import Translate from "./pages/Translate";
 import Page404 from './pages/Page404'
-import Nav from  "./components/Nav"
-import Home from './pages/home'
-import Translate from './pages/Translate'
-import Grammar from './pages/Grammar'
-import Footer from './components/Footer'
-import {useState,useEffect} from 'react'
-import {auth}  from "../src/firebase_config";
+import SignIn from "./pages/SignIn";
+import Register from "./pages/Register";
+import Grammar from "./pages/Grammar";
+import About from './pages/About'
+import Nav from "./components/Nav";
+import Footer from "./components/Footer";
+import { useState, useEffect } from "react";
+import { auth } from "../src/firebase_config";
 import { signInWithPopup, GoogleAuthProvider, signOut } from "firebase/auth";
 import { useNavigate } from "react-router-dom";
-import SignIn from './pages/SignIn'
 function App() {
   const [userInfo, setUserInfo] = useState(null);
 
@@ -28,33 +28,33 @@ function App() {
     });
   }),
     [userInfo];
-  const login = async() => {
-    try{
-        const provider = new GoogleAuthProvider();
-        auth.useDeviceLanguage();
-        const result = await signInWithPopup(auth, provider)
-        useNavigate('/')
-    }catch(err){
-        alert(err.message);
+  const login = async () => {
+    try {
+      const provider = new GoogleAuthProvider();
+      auth.useDeviceLanguage();
+      const result = await signInWithPopup(auth, provider);
+      useNavigate("/");
+    } catch (err) {
+      alert(err.message);
     }
-}
+  };
   return (
     <>
       <BrowserRouter>
-      <Nav user={userInfo} />
+        <Nav user={userInfo} />
         <Routes>
-          <Route path="/" exact element={<Home/>} />
-          <Route path="translate" element={<Translate/>} />
-          <Route path="grammar" element={<Grammar/>} />
-           <Route path='about' element={<About />} />
-           <Route path='SignIn' element={<SignIn />} />
-           <Route path='Register' element={<Register />} />
-          <Route path="*" element={<Page404/>} />
+          <Route path="/" exact element={<Home />} />
+          <Route path="translate" element={<Translate />} />
+          <Route path="grammar" element={<Grammar />} />
+          <Route path="about" element={<About />} />
+          <Route path="SignIn" element={<SignIn />} />
+          <Route path="Register" element={<Register />} />
+          <Route path="*" element={<Page404 />} />
         </Routes>
-        <Footer/>
+        <Footer />
       </BrowserRouter>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
