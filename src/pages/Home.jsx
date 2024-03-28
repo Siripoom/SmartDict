ddb217b6da4b8f02df61a976f77cf4ae65a8e4f7
Siripoom <artorsiriratpoom@gmail.com>
2024-03-28 20:00:08 +0700

    add Home.jsx

---
+++ src/pages/Home.jsx
@@ -0,0 +1,11 @@
+import Hero from '../components/Hero'
+import Word from '../components/Word'
+
+export default function Home(){
+    return(
+        <div className="container">
+            <Hero/>
+            <Word/>
+        </div>
+    )
+}
