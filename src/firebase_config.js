47543f95ac71c574c37406e05f39b6538725942a
Mr.Siripoom Arthornsiriat <65816702+Siripoom@users.noreply.github.com>
2024-12-07 23:03:59 +0700

    Update firebase_config.js

--- src/firebase_config.js
+++ src/firebase_config.js
@@ -9,12 +9,12 @@ import SignIn from "./pages/SignIn";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyCPf0o9DLFJmNZQsBdhgF-zB6r6TljfnWw",
-  authDomain: "smartdict-b054d.firebaseapp.com",
-  projectId: "smartdict-b054d",
-  storageBucket: "smartdict-b054d.appspot.com",
-  messagingSenderId: "813207595122",
-  appId: "1:813207595122:web:881e455bd93e376a22faeb"
+  apiKey: "",
+  authDomain: "",
+  projectId: "",
+  storageBucket: "",
+  messagingSenderId: "",
+  appId: ""
 };
 
 // Initialize Firebase
